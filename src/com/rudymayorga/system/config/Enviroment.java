f806642dde5fcbfd418ebbc127839f445bb222b2
Rudy Mayorga <rmayorga2026131@kinal.edu.gt>
2026-08-23 21:43:55 -0600

    porfin se fue el enviroment

--- src/com/rudymayorga/system/config/Enviroment.java
+++
@@ -1,9 +0,0 @@
-package com.rudymayorga.system.config;
-
-public class Enviroment {
-    
-    protected static final String USER="IN4AV";
-    protected static final String PASSWORD = "&mnid4AV";
-    protected static final String DATA_BASE ="auditoria_usuario_producto_in4av";
-    protected static final String LOCATION_SERVICE = "localhost:3306";
-}
